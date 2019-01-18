3af93a9ea576328ee613a54bb84ebb73b0a9a5be
Hasanuzzaman <hasanuzzamanbe@gmail.com>
2019-01-18 18:17:02 +0600

    Search engine added successfully

--- background.js
+++ background.js
@@ -1,16 +1,67 @@
-var contextMenuItem = {
-	"id" : "hzByHover",
-	"title" : "Quick Search",
-	"contexts" : ["selection"]
-};
-chrome.contextMenus.create(contextMenuItem)
+
+chrome.contextMenus.create({
+	title: "Quick search on",
+	id: "hzByHover",
+	contexts:["selection"]
+
+});
+chrome.contextMenus.create({
+	"title" : "Google",
+	id: "hzByHoverGoogle",
+	parentId: "hzByHover",
+	contexts:["selection"]
+});
+chrome.contextMenus.create({
+	title: "YouTube",
+	id: "hzByHoverYt",
+	parentId: "hzByHover",
+	contexts:["selection"],
+});
+chrome.contextMenus.create({
+	title: "Bing",
+	id: "hzByHoverBing",
+	parentId: "hzByHover",
+	contexts:["selection"],
+});
+chrome.contextMenus.create({
+	title: "Baidu",
+	id: "hzByHoverBaidu",
+	parentId: "hzByHover",
+	contexts:["selection"],
+});
+chrome.contextMenus.create({
+	title: "Ask",
+	id: "hzByHoverAsk",
+	parentId: "hzByHover",
+	contexts:["selection"],
+});
 chrome.contextMenus.onClicked.addListener((clickData)=>{
-		  if (clickData.menuItemId == "hzByHover" && clickData.selectionText) {
-		  	var google = 'http://google.com/search?q='
-		  	var googleTube = "https://www.youtube.com/results?search_query="
-		  	var link = googleTube + clickData.selectionText
-			    window.open(link)
+		  if (clickData.menuItemId == "hzByHoverGoogle" && clickData.selectionText) {
+		  	let google = 'http://google.com/search?q='
+		  	let googleTube = "https://www.youtube.com/results?search_query="
+		  	let link = google + clickData.selectionText
+			    window.open(link);
 		    }
-        
-		})
+			if (clickData.menuItemId == "hzByHoverYt" && clickData.selectionText) {
+				let YouTube = "https://www.youtube.com/results?search_query="
+				let YouTubelink = YouTube + clickData.selectionText
+				window.open(YouTubelink);
+			}
+			if (clickData.menuItemId == "hzByHoverBing" && clickData.selectionText) {
+			let Bing = "https://www.bing.com/search?q="
+			let Binglink = Bing + clickData.selectionText
+			window.open(Binglink);
+			}
+			if (clickData.menuItemId == "hzByHoverBaidu" && clickData.selectionText) {
+				let Baidu = "https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=baidu&wd="
+				let Baidulink = Baidu + clickData.selectionText
+				window.open(Baidulink);
+			}
+			if (clickData.menuItemId == "hzByHoverAsk" && clickData.selectionText) {
+				let Ask = "https://www.search.ask.com/web?q="
+				let Asklink = Ask + clickData.selectionText
+				window.open(Asklink);
+			}
+
+})
 
--- popup.js
+++ popup.js
@@ -1,13 +1,14 @@
 
-//  let checkBtn = document.getElementById('checkBtn');
 
-//   checkBtn.onclick = function(element) {
-//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-// 	      chrome.tabs.executeScript(
-// 	          tabs[0].id,  
-// 	          {code: `
-// 	              alert('hello there')
-// 	          `}
-// 	        );
-//     });
-//   }
+let checkBtn = document.getElementById('checkBtn');
+
+ checkBtn.onclick = function(element) {
+     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+         chrome.tabs.executeScript(
+             tabs[0].id,
+             {code: `
+	         window.open('www.google.com')
+	          	`});
+
+     });
+ }
